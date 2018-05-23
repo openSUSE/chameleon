352099461c715078baa83e6aae83fc1c693652cb
Guo Yunhe <guoyunhebrave@gmail.com>
2018-05-23 15:07:03 +0300

    Update gulpfile.js , add sourcemap and minify code. Remove small packages.

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,46 @@
 "use strict";
 
 var gulp = require("gulp");
-var browserify = require("gulp-browserify");
+var browserify = require("browserify");
+var log = require("gulplog");
+var tap = require("gulp-tap");
+var buffer = require("gulp-buffer");
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
 var sass = require("gulp-sass");
 
-// Compile JavaScripts
+// Compile JavaScripts with sourcemaps
 gulp.task("js", function() {
-  // Single entry point to browserify
-  gulp
-    .src(["src/js/app.js", "src/js/app-no-jquery.js"])
+  return gulp
+    .src("src/js/**/*.js", { read: false })
     .pipe(
-      browserify({
-        insertGlobals: true,
-        debug: true
+      tap(function(file) {
+        log.info("bundling " + file.path);
+        file.contents = browserify(file.path, { debug: true }).bundle();
       })
     )
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./js"));
 });
 
-// Compile SASS stylesheets
+// Compile SaSS stylesheets with sourcemaps
 gulp.task("sass", function() {
   return gulp
     .src("./src/sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./css"));
 });
 
-gulp.task("copy", function() {
-  return gulp
-    .src(["node_modules/typicons.font/src/font/typicons.*"])
-    .pipe(gulp.dest("./fonts/typicons"));
-});
-
 // Build all
-gulp.task("default", ["js", "sass", "copy"]);
+gulp.task("default", gulp.parallel("js", "sass"));
 
 // Watch all
 gulp.task("watch", function() {
-  gulp.watch("./src/sass/**/*.scss", ["sass"]);
-  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/sass/**/*.scss", gulp.parallel("sass"));
+  gulp.watch("./src/js/**/*.js", gulp.parallel("js"));
 });
