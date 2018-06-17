8c5fa95271824b2b12124c2b82544803f8107be4
Guo Yunhe <guoyunhebrave@gmail.com>
2018-06-17 14:00:22 +0300

    Language select

--- src/js/chameleon-no-jquery.js
+++ src/js/chameleon-no-jquery.js
@@ -3,4 +3,5 @@ window.Tether = require("tether");
 window.Popper = require("popper.js");
 require("bootstrap");
 
+require("./localization/language-select");
 require("./localization/navbar-footer");
---
+++ src/js/localization/language-select.js
@@ -0,0 +1,68 @@
+var languages = {
+  ar: "العربية",
+  bg: "Български",
+  ca: "Català",
+  cs: "Čeština",
+  da: "Dansk",
+  de: "Deutsch",
+  el: "Ελληνικά",
+  en: "English",
+  es: "Español",
+  fa: "فارسی",
+  fi: "Suomi",
+  fr: "Français",
+  gl: "Galego",
+  hi: "हिन्दी",
+  hu: "Magyar",
+  id: "Bahasa Indonesia",
+  it: "Italiano",
+  ja: "日本語",
+  km: "ភាសាខ្មែរ",
+  ko: "한국어",
+  lt: "Lietuvių",
+  nb: "Bokmål",
+  nl: "Nederlands",
+  nn: "Nynorsk",
+  pl: "Polski",
+  "pt-BR": "Português",
+  ro: "Română",
+  ru: "Русский",
+  sk: "Slovenčina",
+  sv: "Svenska",
+  th: "ภาษาไทย",
+  uk: "Українська",
+  wa: "Walon",
+  "zh-CN": "简体中文",
+  "zh-TW": "繁體中文"
+};
+
+$(function() {
+  /**
+   * Language detect order:
+   * 1. Users' choice saved in local storage
+   * 2. First matched browser language
+   * 3. Use English as fallback language
+   */
+  var language =
+    localStorage.getItem("lang") ||
+    navigator.languages.find(function(lang) {
+      return lang in languages;
+    }) ||
+    "en";
+
+  document.documentElement.lang = language;
+
+  for (var lang in languages) {
+    var $option = $("<option></option>")
+      .val(lang)
+      .text(languages[lang])
+      .attr("selected", language === lang);
+    $("#language-select").append($option);
+  }
+
+  $("select#language-select").change(function() {
+    var lang = $(this).val();
+    document.documentElement.lang = lang;
+    localStorage.setItem("lang", lang);
+  });
+});
