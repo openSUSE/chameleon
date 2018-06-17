cf2c29ef6398b36a2b7999434e876f4ff5929416
Guo Yunhe <guoyunhebrave@gmail.com>
2018-06-17 14:21:08 +0300

    Update translated string when changing language

--- src/js/localization/localize.js
+++ src/js/localization/localize.js
@@ -1,31 +1,45 @@
 function localize(selector, translations) {
-  /**
-   * Detect language
-   */
-  var lang = document.documentElement.lang;
+  function localizeString() {
+    /**
+     * Detect language
+     */
+    var lang = document.documentElement.lang;
 
-  if (!lang || !(lang in translations)) {
-    lang = "en"; // Use English if it is not a supported locale
+    if (!lang || !(lang in translations)) {
+      lang = "en"; // Use English if it is not a supported locale
+    }
+
+    var translation = translations[lang];
+
+    /**
+     * Replace translateable string
+     */
+    $(function() {
+      $(selector).each(function() {
+        if ($(this).data("msg-id")) {
+          var text = translation[$(this).data("msg-id")];
+          $(this).text(text);
+        }
+
+        if ($(this).data("url-id")) {
+          var url = translation[$(this).data("url-id")];
+          $(this).attr("href", url);
+        }
+      });
+    });
   }
 
-  var translation = translations[lang];
+  localizeString();
 
-  /**
-   * Replace translateable string
-   */
-  $(function() {
-    $(selector).each(function() {
-      if ($(this).data("msg-id")) {
-        var text = translation[$(this).data("msg-id")];
-        $(this).text(text);
-      }
-
-      if ($(this).data("url-id")) {
-        var url = translation[$(this).data("url-id")];
-        $(this).attr("href", url);
+  var observer = new MutationObserver(function(mutationsList) {
+    mutationsList.map(function(mutation) {
+      if (mutation.type === "attributes" && mutation.attributeName === "lang") {
+        localizeString();
       }
     });
   });
+
+  observer.observe(document.documentElement, { attributes: true });
 }
 
 module.exports = localize;
