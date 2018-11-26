774aca02f826cc6c0b8b613f43448bff1bd0f27e
Guo Yunhe <i@guoyunhe.me>
2018-11-26 20:30:32 +0200

    Load Russian and German languages

--- src/js/localization/navbar-footer.js
+++ src/js/localization/navbar-footer.js
@@ -3,13 +3,7 @@
  */
 
 var localize = require("./localize");
-
-var translations = {};
-
-translations["en"] = require("../../langs/en.json");
-translations["pt-BR"] = require("../../langs/pt_BR.json");
-translations["zh-CN"] = require("../../langs/zh_CN.json");
-translations["zh-TW"] = require("../../langs/zh_TW.json");
+var translations = require('./translations');
 
 document.addEventListener('DOMContentLoaded', function () {
   localize("#global-navbar .l10n, #global-footer .l10n", translations);
---
+++ src/js/localization/translations.js
@@ -0,0 +1,10 @@
+var translations = {};
+
+translations["de"] = require("../../langs/de.json");
+translations["en"] = require("../../langs/en.json");
+translations["pt-BR"] = require("../../langs/pt_BR.json");
+translations["ru"] = require("../../langs/ru.json");
+translations["zh-CN"] = require("../../langs/zh_CN.json");
+translations["zh-TW"] = require("../../langs/zh_TW.json");
+
+module.exports = translations;
