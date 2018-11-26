e83111ea17f5a221760cad1f9addca11b2ad25ac
Guo Yunhe <i@guoyunhe.me>
2018-11-26 20:42:41 +0200

    Avoid jQuery in wiki loader function

--- src/js/wiki/index.js
+++ src/js/wiki/index.js
@@ -1,4 +1,4 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function () {
   if (document.body.classList.contains('mediawiki')) {
     require("./login-modal");
     require("./toc");
