afac67478be8aa889c52bdca8e8423b96b7303a0
Guo Yunhe <i@guoyunhe.me>
2019-07-30 23:13:55 +0300

    wiki-container layout

--- src/js/wiki/toc.js
+++ src/js/wiki/toc.js
@@ -3,13 +3,13 @@
 var StickyScroller = require("sticky-scroller");
 var toc = document.getElementById("toc");
 var sidebar = document.getElementById("toc-sidebar");
+var main = document.getElementById("main");
 
 if (sidebar) {
   if (toc) {
     sidebar.append(toc);
+    sidebar.classList.add('d-lg-block', 'col-lg-3');
+    main.classList.add('col-lg-9');
     new StickyScroller(toc);
-  } else {
-    sidebar.classList.remove("d-md-block");
-    sidebar.classList.add("d-xl-block");
   }
 }
