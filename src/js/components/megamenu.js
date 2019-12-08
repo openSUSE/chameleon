f00c235406abff62826b125c04974ef535671d65
Guo Yunhe <i@guoyunhe.me>
2019-12-08 04:05:54 +0200

    jQuery

--- src/js/components/megamenu.js
+++ src/js/components/megamenu.js
@@ -1,33 +1,33 @@
 const sections = [
   {
-    id: 'main',
+    id: "main",
     title: "Main",
     links: [
       {
         id: "main-site",
         title: "Main site",
-        url: "https://www.opensuse.org/",
+        url: "https://www.opensuse.org/"
       },
       {
         id: "software",
         title: "Software",
-        url: "https://software.opensuse.org/",
+        url: "https://software.opensuse.org/"
       },
       {
         id: "wiki",
         title: "Wiki",
-        url: "https://en.opensuse.org/",
+        url: "https://en.opensuse.org/"
       },
       {
         id: "documentation",
         title: "Documentation",
-        url: "https://doc.opensuse.org/",
+        url: "https://doc.opensuse.org/"
       },
       {
         id: "forum",
         title: "Forum",
-        url: "https://forums.opensuse.org/",
-      },
+        url: "https://forums.opensuse.org/"
+      }
     ]
   },
   {
@@ -37,28 +37,28 @@ const sections = [
       {
         id: "build-service",
         title: "Build service",
-        url: "https://build.opensuse.org/",
+        url: "https://build.opensuse.org/"
       },
       {
         id: "bugzilla",
         title: "Bugzilla",
-        url: "https://bugzilla.opensuse.org/",
+        url: "https://bugzilla.opensuse.org/"
       },
       {
         id: "github",
         title: "GitHub",
-        url: "https://github.com/opensuse",
+        url: "https://github.com/opensuse"
       },
       {
         id: "openaq",
         title: "openQA",
-        url: "https://openqa.opensuse.org/",
+        url: "https://openqa.opensuse.org/"
       },
       {
         id: "weblate",
         title: "Weblate",
-        url: "https://l10n.opensuse.org/",
-      },
+        url: "https://l10n.opensuse.org/"
+      }
     ]
   },
   {
@@ -68,28 +68,28 @@ const sections = [
       {
         id: "news",
         title: "News",
-        url: "https://news.opensuse.org/",
+        url: "https://news.opensuse.org/"
       },
       {
         id: "events",
         title: "Events",
-        url: "https://events.opensuse.org/",
+        url: "https://events.opensuse.org/"
       },
       {
         id: "planet",
         title: "Planet",
-        url: "https://planet.opensuse.org/",
+        url: "https://planet.opensuse.org/"
       },
       {
         id: "shop",
         title: "Shop",
-        url: "https://shop.opensuse.org/",
+        url: "https://shop.opensuse.org/"
       },
       {
         id: "status",
         title: "Status",
-        url: "https://status.opensuse.org/",
-      },
+        url: "https://status.opensuse.org/"
+      }
     ]
   },
   {
@@ -99,28 +99,28 @@ const sections = [
       {
         id: "irc-channels",
         title: "IRC channels",
-        url: "https://en.opensuse.org/openSUSE:IRC_list",
+        url: "https://en.opensuse.org/openSUSE:IRC_list"
       },
       {
         id: "mail-lists",
         title: "Mail lists",
-        url: "https://en.opensuse.org/openSUSE:Mailing_lists_subscription",
+        url: "https://en.opensuse.org/openSUSE:Mailing_lists_subscription"
       },
       {
         id: "facebook-group",
         title: "Facebook group",
-        url: "https://www.facebook.com/groups/opensuseproject",
+        url: "https://www.facebook.com/groups/opensuseproject"
       },
       {
         id: "telegram-group",
         title: "Telegram group",
-        url: "https://t.me/opensuseusers",
+        url: "https://t.me/opensuseusers"
       },
       {
         id: "reddit",
         title: "Reddit",
-        url: "https://reddit.com/r/openSUSE",
-      },
+        url: "https://reddit.com/r/openSUSE"
+      }
     ]
   },
   {
@@ -130,28 +130,28 @@ const sections = [
       {
         id: "mastodon",
         title: "Mastodon",
-        url: "https://fosstodon.org/@opensuse",
+        url: "https://fosstodon.org/@opensuse"
       },
       {
         id: "telegram",
         title: "Telegram",
-        url: "https://t.me/opensusenews",
+        url: "https://t.me/opensusenews"
       },
       {
         id: "facebook",
         title: "Facebook",
-        url: "https://www.facebook.com/en.openSUSE",
+        url: "https://www.facebook.com/en.openSUSE"
       },
       {
         id: "twitter",
         title: "Twitter",
-        url: "https://twitter.com/opensuse",
+        url: "https://twitter.com/opensuse"
       },
       {
         id: "youtube",
         title: "YouTube",
-        url: "https://www.youtube.com/user/opensusetv",
-      },
+        url: "https://www.youtube.com/user/opensusetv"
+      }
     ]
   },
   {
@@ -160,30 +160,30 @@ const sections = [
       {
         id: "packman",
         title: "Packman",
-        url: "http://packman.links2linux.org/",
+        url: "http://packman.links2linux.org/"
       },
       {
         id: "kubic",
         title: "Kubic",
-        url: "https://kubic.opensuse.org/",
+        url: "https://kubic.opensuse.org/"
       },
       {
         id: "guide-unofficial",
         title: "Guide (unofficial)",
-        url: "https://opensuse-guide.org/",
+        url: "https://opensuse-guide.org/"
       },
       {
         id: "mirrors",
         title: "Mirrors",
-        url: "https://mirrors.opensuse.org/",
+        url: "https://mirrors.opensuse.org/"
       },
       {
         id: "lizards",
         title: "Lizards",
-        url: "https://lizards.opensuse.org/",
-      },
+        url: "https://lizards.opensuse.org/"
+      }
     ]
-  },
+  }
 ];
 
 const modal = document.createElement("div");
@@ -192,18 +192,22 @@ modal.setAttribute("tabindex", "-1");
 modal.setAttribute("role", "dialog");
 document.body.prepend(modal);
 
-const content = sections.map(function (section) {
-  const links = section.links.map(function (link) {
-    return `<p><a class="l10n" href="${link.url}" data-msg-id="${link.id}" data-url-id="${link.id}-url">${link.title}</a></p>`;
-  }).join('');
+const content = sections
+  .map(function(section) {
+    const links = section.links
+      .map(function(link) {
+        return `<p><a class="l10n" href="${link.url}" data-msg-id="${link.id}" data-url-id="${link.id}-url">${link.title}</a></p>`;
+      })
+      .join("");
 
-  return `
+    return `
   <div class="col-6 col-md-4 mb-5">
     <h6 class="megamenu-heading l10n" data-msg-id="${section.id}">${section.title}</h6>
     ${links}
   </div>
-  `
-}).join('');
+  `;
+  })
+  .join("");
 
 modal.innerHTML = `
 <div class="modal-dialog modal-dialog-centered modal-lg" role=document">
@@ -229,9 +233,11 @@ modal.innerHTML = `
 </div>
 `;
 
-const $modal = jQuery(modal);
-$modal.modal("hide");
+document.addEventListener("readystatechange", function() {
+  const $modal = jQuery(modal);
+  $modal.modal("hide");
 
-jQuery(".megamenu-toggler").click(function () {
-  $modal.modal("show");
-})
+  jQuery(".megamenu-toggler").click(function() {
+    $modal.modal("show");
+  });
+});
