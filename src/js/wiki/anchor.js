d4a0f4324bee0698c31982401a3bd98b5e72e461
Guo Yunhe <i@guoyunhe.me>
2020-03-03 17:03:01 +0200

    wiki: fix heading anchor

--- src/js/wiki/anchor.js
+++ src/js/wiki/anchor.js
@@ -4,7 +4,7 @@ const headlines = document.getElementsByClassName("mw-headline");
 
 for (let i = 0; i < headlines.length; i++) {
   const headline = headlines.item(i);
-  const link = document.createElement(a);
+  const link = document.createElement("a");
   link.href = "#" + headline.id;
   link.innerHTML = '<svg class="icon"><use xlink:href="#links-line"></svg>';
   headline.after(link);
