00c01c91f96889e42dfebd9ce63e2193a1cdf705
Guo Yunhe <i@guoyunhe.me>
2020-06-15 17:37:24 +0300

    update wiki heading icon

--- dist/js/chameleon-wiki.js
+++ dist/js/chameleon-wiki.js
@@ -19,7 +19,8 @@ for (let i = 0; i < headlines.length; i++) {
 
   const link = document.createElement("a");
   link.href = "#" + headline.id;
-  link.innerHTML = '<svg class="icon"><use xlink:href="#links"></svg>';
+  link.innerHTML =
+    '<svg class="bi bi-link-45deg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/><path d="M5.712 6.96l.167-.167a1.99 1.99 0 0 1 .896-.518 1.99 1.99 0 0 1 .518-.896l.167-.167A3.004 3.004 0 0 0 6 5.499c-.22.46-.316.963-.288 1.46z"/><path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/><path d="M10 9.5a2.99 2.99 0 0 0 .288-1.46l-.167.167a1.99 1.99 0 0 1-.896.518 1.99 1.99 0 0 1-.518.896l-.167.167A3.004 3.004 0 0 0 10 9.501z"/></svg>';
 
   wrap.append(link);
   headline.after(wrap);
@@ -34,7 +35,8 @@ const wraps = document.getElementsByClassName("mw-editsection");
 for (let i = 0; i < wraps.length; i++) {
   const wrap = wraps.item(i);
   const link = wrap.getElementsByTagName("a").item(0);
-  link.innerHTML = '<svg class="icon"><use xlink:href="#pencil"></svg>';
+  link.innerHTML =
+    '<svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/><path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/></svg>';
 }
 
 },{}],4:[function(require,module,exports){
